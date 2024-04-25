1b3887841394a95e4d313e5270c23d0dbabce8f7
Matsugawa-Rikiya <m-rikiya-2412@occ.co.jp>
2024-04-25 13:35:01 +0900

    テスト

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -52,40 +52,83 @@ function HomePage() {
       </header>
 
       {/* メインコンテンツ */}
+      <div className="text-center mb-6 leading-relaxed">
+        <main className="flex-1 container mx-auto px-4 py-8">
+          <img
+            src="images/white.png"
+            className="object-cover h-1/4 w-full border rounded-lg shadow-lg"
+          />
 
-      <main className="flex-1 container mx-auto px-4 py-8">
-        <img
-          src="images/white.png"
-          className="object-cover h-1/4 w-full border rounded-lg shadow-lg"
-        />
+          <div className="w-11/12 md:w-10/12 lg:w-1/2 mx-auto mb-6 leading-relaxed">
+            <div className="bg-blue-100 inline-block py-1 px-4 rounded-xl mb-6">
+              <p className="text-blue-500 font-bold ">Update</p>
+            </div>
 
-        <div className="w-11/12 md:w-10/12 lg:w-1/2 mx-auto">
-          <div className="bg-blue-100 inline-block py-1 px-4 rounded-xl mb-6">
-            <p className="text-blue-500 font-bold">Update</p>
+            <h1 className="text-3xl md:text-5xl text-ui-secondary font-semibold">
+              TailwindTemplates v2 is here
+            </h1>
+            <p className="text-ui-secondary leading-8"> 1 min read</p>
           </div>
-
-          <h1 className="text-3xl md:text-5xl text-ui-secondary font-semibold">
-            TailwindTemplates v2 is here
-          </h1>
-          <p className="text-ui-secondary leading-8"> 1 min read</p>
-        </div>
-        <div className="mb-6 leading-relaxed">
-          <p>
-            TailwindTemplates V2 is finally here! Our focus for
+          <div className="break-words text-3xll mb-6 leading-relaxed">
+            TailwindTemplates V2 is finally here! Our focus for open-source
+            design systems to Tailwindcss. Each design system has a guideline to
+            provide consistency and coherence within a user
+          </div>
+          <h1>What to expect?</h1>
+          <p className="whitespace-nowrap ">
+            Over the coming months, we will expand the design systems that are
+            currently available at launch. In addition to that, we are planning
+            to create several tailwind configurations to easily get started with
+            the proper colors and variants that belong to each design system.
           </p>
-          <p>
-            open-source design systems to Tailwindcss. Each design system has a
-          </p>
-          <p>
-            guideline to provide consistency and coherence within a user
-          </p>
-        </div>
-      </main>
+        </main>
+      </div>
 
       {/* フッター */}
-      <footer className=" sm:hidden sticky bottom-0 bg-white py-2 border ">
-        <div className="container mx-auto px-4 py-4 text-center text-gray-600"></div>
-      </footer>
+      {/* -------------------------footer---------------------------------- */}
+      <div className="bg-blue-700 ">
+        <div className="mx-auto max-w-5xl  pt-1 px-4 sm:px-6 lg:px-8">
+          <div className="flex flex-wrap  mt-20 ustifjy-between">
+            {/* <!-- 列 1 --> */}
+            <div className=" w-full  sm:w-1/2 md:w-1/3">
+              <h4 className="text-gray-100">About</h4>
+              <p className="text-gray-100">Me</p>
+              <p className="text-gray-100">Contact</p>
+              <p className="text-gray-100">Disclaimer</p>
+            </div>
+            {/* <!-- 列 2 --> */}
+            <div className=" w-full  sm:w-1/2 md:w-1/3">
+              <h4 className="text-gray-100">About</h4>
+              <p className="text-gray-100">frsfwrg</p>
+              <p className="text-gray-100">talleded</p>
+            </div>
+            {/* <!-- 列 3 --> */}
+            <div className=" w-full  sm:w-1/2 md:w-1/3">
+              <button className="bg-red-500 text-white px-20 py-2 rounded">
+                ボタン
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <p></p>
+        <br />
+        <p></p>
+        <br />
+        <br />
+        <br />
+        <div className="text-gray-100 text-center">
+          <button className="bg-red-500 text-white px-20 py-2 rounded">
+            ボタン
+          </button>
+        </div>
+        <br />
+        <p className="text-gray-100 text-center">
+          Built copylight with offey @ by Jucyhiz
+        </p>
+        <br />
+        <br />
+      </div>
     </div>
   );
 }
