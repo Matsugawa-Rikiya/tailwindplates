2c0a2c6d9ea2546787f326f140a7aa0c92c407d4
Matsugawa-Rikiya <m-rikiya-2412@occ.co.jp>
2024-04-26 10:34:01 +0900

    aa

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -58,7 +58,6 @@ function HomePage() {
             src="images/white.png"
             className="object-cover h-1/4 w-full border rounded-lg shadow-lg"
           />
-
           <div className="w-11/12 md:w-10/12 lg:w-1/2 mx-auto mb-6 leading-relaxed">
             <div className="bg-blue-100 inline-block py-1 px-4 rounded-xl mb-6">
               <p className="text-blue-500 font-bold ">Update</p>
@@ -69,9 +68,13 @@ function HomePage() {
             </h1>
             <p className="text-ui-secondary leading-8"> 1 min read</p>
           </div>
-          <div className="break-words text-3xll mb-6 leading-relaxed">
+          <div className="break-words text-3xll mb-6 leading-relaxed center">
             TailwindTemplates V2 is finally here! Our focus for open-source
+          </div>
+          <div className="break-words text-3xll mb-6 leading-relaxed center">
             design systems to Tailwindcss. Each design system has a guideline to
+          </div>
+          <div className="break-words text-3xll mb-6 leading-relaxed center">
             provide consistency and coherence within a user
           </div>
           <h1>What to expect?</h1>
